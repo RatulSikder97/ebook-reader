2650861c84f365824d0c14ebf135c9dd3bdc7e63
Ratul <ratul.sikder@rokomari.com>
2021-08-12 12:32:08 +0600

    Fix Dom Parser issue

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-book',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private bookService: BookService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  readBook(event) {
+
+    this.bookService.openBook(event.target.files[0])
+
   }
 
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import ePub,{ Book, Rendition } from 'epubjs';
+import { RenditionOptions } from 'epubjs/types/rendition';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  book: Book = ePub();
+  rendition!: Rendition;
+
+  constructor() {
+    
+   }
+
+   openBook(input: any, what?: string) {
+    this.book.open(input, what);
+    console.log(this.book)
+  }
+
+  display(target?: string) {
+    this.rendition.display(target);
+  }
+
+  renderTo(element: string, options: RenditionOptions) {
+    this.rendition = this.book.renderTo(element, options);
+  }
 }
