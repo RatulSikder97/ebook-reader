8a2bb31fdb74064115995efede12cb8d4ad66d88
Ratul <ratul.sikder@rokomari.com>
2021-08-16 10:01:23 +0600

    Firebase integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,22 @@ import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { reducers, metaReducers } from './store';
 
+// firebase module import
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBozXuKzldrc83CdGYHyFW3OESOBTbHjdI",
+  authDomain: "e-book-8621b.firebaseapp.com",
+  projectId: "e-book-8621b",
+  storageBucket: "e-book-8621b.appspot.com",
+  messagingSenderId: "274765546967",
+  appId: "1:274765546967:web:edf9a8f67bda33e963b792"
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +32,13 @@ import { reducers, metaReducers } from './store';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+
+
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     EffectsModule.forRoot([]),
